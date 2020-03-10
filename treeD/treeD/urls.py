8f62318468ff2cc5e43d2a26e0bd991ad1d55433
sergioperez1998 <sergio-pm@hotmail.com>
2020-03-10 01:20:04 +0100

    feature #18 terminado backend del login, faltan los settings cuando este terminado el index

--- treeD/treeD/urls.py
+++ treeD/treeD/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(), name="login_url"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

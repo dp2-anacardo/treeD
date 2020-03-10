a7197b143810e83b62958eea8265dc573b3a280d
sergioperez1998 <sergio-pm@hotmail.com>
2020-03-10 01:01:24 +0100

    feature #17 terminado el codigo backend

--- treeD/main/views.py
+++ treeD/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from main.models import Impresion, Perfil, Imagen, Compra
 
 # Create your views here.
+
+def listarComprasImpresiones(request):
+
+    usuario = request.user
+    perfil = Perfil.objects.all().filter(usuario = usuario)
+    comprasRealizadas = perfil.impresionesCompradas
+    return render(request, 'impresiones/listarCompras.html', {'impresiones':comprasRealizadas})
--- treeD/treeD/urls.py
+++ treeD/treeD/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('impresion/listarCompras/', views.listarComprasImpresiones, name="listarComprasRealizas_url"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

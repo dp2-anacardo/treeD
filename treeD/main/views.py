2c7ac4e4a32038d8e58a52358f528f9fc00d64d2
sergioperez1998 <sergio-pm@hotmail.com>
2020-03-10 20:45:32 +0100

    feature #17 arreglando bug

--- treeD/main/views.py
+++ treeD/main/views.py
@@ -16,13 +16,20 @@ def listarComprasImpresiones(request):
     try:
         usuario = usuarioLogueado(request)
         idUsuario = usuario.idPerfil
-        compras = Compra.objects.all().filter(idPerfil = idUsuario)
+        compras = list(Compra.objects.all().filter(idPerfil = idUsuario))
         impresiones=[]
+        diccionario={}
         for c in compras:
-            idImpresion = c.idImpresion
-            impresioncompra= Impresion.objects.get(idImpresion = idImpresion.idImpresion)
-            impresiones.append(impresioncompra)
-        return render(request, 'impresiones/listarCompras.html', {'compras':compras, 'impresiones':impresiones})
+            impresionCompra= Impresion.objects.get(idImpresion = c.idImpresion.idImpresion)
+            impresiones.append(impresionCompra)
+        for c in compras:
+            for i in impresiones:
+                if c.idImpresion.idImpresion == i.idImpresion:
+                    diccionario[c]=i
+                    impresiones.remove(i)
+                    break
+
+        return render(request, 'impresiones/listarCompras.html', {'diccionario':diccionario})
     except:
         return redirect('error_url')
 

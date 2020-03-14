2df350021020876214ccae042e69487c40d7005b
Servesus <jesusgarrocho1998@gmail.com>
2020-03-14 16:45:31 +0100

    Issue #18 terminada

--- treeD/treeD/urls.py
+++ treeD/treeD/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,LogoutView
 from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index),
-    path('base.html/', LoginView.as_view(), name="login_url")
+    path('login/', LoginView.as_view(), name="login_url"),
+    path('logout/', LogoutView.as_view(), name="logout_url"),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 

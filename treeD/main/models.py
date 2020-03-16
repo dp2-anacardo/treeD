4f9cc3a8198bf5fce9d34c940e0ffd2dba319939
unknown <miguelvr98@gmail.com>
2020-03-16 16:19:06 +0100

    Probando

--- treeD/main/models.py
+++ treeD/main/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Imagen(models.Model):
     idImagen = models.AutoField(primary_key=True)
     imagen = models.ImageField(upload_to='imagenes',verbose_name='Imagen')
-    impresion = models.ForeignKey('Impresion', on_delete=models.CASCADE)
+    impresion = models.ForeignKey('Impresion', on_delete=models.SET_NULL, null= True)
 
     def __str__(self):
         return str(self.idImagen)

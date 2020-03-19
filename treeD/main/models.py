c1d422ad27f923e3a51330fc34973b5ac5664002
ManuErCaximba <ercaximbero96@gmail.com>
2020-03-19 23:00:48 +0100

    Actualizado el modelo relacional #43, tambien corregido pequeño bug en categorias

--- treeD/main/models.py
+++ treeD/main/models.py
@@ -21,6 +21,16 @@ class ImgImpresion(models.Model):
     class Meta:
         ordering = ('pk',)
 
+class DirecPerfil(models.Model):
+    direccion = models.TextField(verbose_name='Dirección')
+    perfil = models.ForeignKey('Perfil', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.direccion
+
+    class Meta:
+        ordering = ('pk',)
+
 class Categoria(models.Model):
     nombre = models.TextField(verbose_name='Nombre')
 
@@ -45,6 +55,11 @@ class Impresion(models.Model):
 
 class Perfil(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
+    nombre = models.TextField(verbose_name='Nombre')
+    apellidos = models.TextField(verbose_name='Apellidos')
+    descripcion = models.TextField(verbose_name='Descripción', null=True)
+    imagen = models.ImageField(upload_to='', verbose_name='Imagen')
+    es_afiliado = models.BooleanField(verbose_name='Afiliado?')
     
     def __str__(self):
         return self.usuario.username
@@ -65,3 +80,19 @@ class Compra(models.Model):
 
     class Meta:
         ordering = ('pk', 'fecha_compra', )
+
+class Presupuesto(models.Model):
+    interesado = models.ForeignKey(Perfil, related_name='Interesado', on_delete=models.SET_NULL, null=True)
+    vendedor = models.ForeignKey(Perfil, related_name='Vendedor', on_delete=models.SET_NULL, null=True)
+    peticion = models.TextField(verbose_name='Peticion')
+    precio = models.FloatField(verbose_name='Precio', null=True)
+    notas = models.TextField(verbose_name='Notas', null=True)
+    fecha_entrega = models.DateField(verbose_name="Fecha de entrega", null=True)
+    resp_interesado = models.BooleanField(verbose_name='Respuesta del Interesado', null=True)
+    resp_vendedor = models.BooleanField(verbose_name='Respuesta del Vendedor', null=True)
+
+    def __str__(self):
+        return self.peticion
+    
+    class Meta:
+        ordering = ('pk', )
